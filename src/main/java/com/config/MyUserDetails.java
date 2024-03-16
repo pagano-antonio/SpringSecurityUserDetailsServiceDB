e932958ba72c0050794e6dc61ad29e6b6de19dff
pagano-antonio <iatix1@gmail.com>
2024-03-16 14:44:05 +0100

    sdf

--- src/main/java/com/config/MyUserDetails.java
+++ src/main/java/com/config/MyUserDetails.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.model.Customer;
@@ -21,6 +24,8 @@ public class MyUserDetails  implements UserDetailsService {
     @Autowired
     private CustomerRepository customerRepository;
 
+
+	
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         String userName, password;
